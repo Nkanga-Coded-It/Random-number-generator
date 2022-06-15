5813c930436ba41d353105dcde8420390633a34b
Nkanga Richard <75099089+Nkanga-Coded-It@users.noreply.github.com>
2022-06-15 15:04:12 +0100

    Initial Commit

---
+++ Shared/Random_number_generatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Random_number_generatorApp.swift
+//  Shared
+//
+//  Created by Richard Nkanga on 15/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Random_number_generatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
